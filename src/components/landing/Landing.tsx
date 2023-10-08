b47757fd06a945cb981fec03bade7922b229b67c
arp <adithya.pai@cognitiveclouds.com>
2023-10-09 01:02:22 +0530

    route animation final,working on right nav

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import React, { useLayoutEffect } from "react";
 import { NavigateFunction } from "react-router-dom";
-import "./Landing.css";
+import AnimatedDiv from "../common/AnimatedDiv";
+import "./Landing.scss";
 
 type Props = {
   navigate: NavigateFunction;
@@ -16,11 +17,11 @@ export default function Landing({ navigate }: Props) {
   const btnRef: any = React.createRef();
 
   const handleClick = () => {
-    const div = introRef.current;
-    div.classList.toggle("intro-flip");
-    setTimeout(() => {
+    // const div = introRef.current;
+    // div.classList.toggle("intro-flip");
+    // setTimeout(() => {
       navigate("/portfolio");
-    }, 1500);
+    // }, 1500);
   };
 
   useLayoutEffect(() => {
@@ -81,38 +82,38 @@ export default function Landing({ navigate }: Props) {
   }, [nameRef2]);
 
   return (
-    <div className="container">
-      <div className="intro-back"></div>
-      <div ref={introRef} className="intro-front">
-        <div ref={lineRef} className="line-top" />
-        <div ref={lineRef1} className="line-down" />
-        <div className="intro-text">
-          <h1 className="name-text">
-            <span ref={nameRef} className="name">
-              Hello,{" "}
-            </span>
-          </h1>
-          <h1 className="name-text">
-            <span ref={nameRef1} className="name">
-              I am
-            </span>
-          </h1>
-          <h1 className="name-text">
-            <span ref={nameRef2} className="name">
-              Adithya
-            </span>
-          </h1>
-        </div>
-        <div ref={btnRef} className="btn-container">
-          <button className="down-btn" onClick={handleClick}>
-            <div className="btn-topline" />
-            <h1 className="btn-text">
-              <span className="port-text">portfolio</span>
+    <AnimatedDiv initial={{x:"-100%",transition:{duration:.4,ease:"linear"}}} animate={{x:"0%",transition:{duration:.4,ease:"linear"}}} exit={{x:"-100%",transition:{duration:.4,ease:"linear"}}} className="container">
+        <div className="intro-back"></div>
+        <div ref={introRef} className="intro-front">
+          <div ref={lineRef} className="line-top" />
+          <div ref={lineRef1} className="line-down" />
+          <div className="intro-text">
+            <h1 className="name-text">
+              <span ref={nameRef} className="name">
+                Hello,{" "}
+              </span>
             </h1>
-            <div className="btn-bottomline" />
-          </button>
+            <h1 className="name-text" style={{}}>
+              <span ref={nameRef1} className="name">
+                I am
+              </span>
+            </h1>
+            <h1 className="name-text">
+              <span ref={nameRef2} className="name">
+                Adithya
+              </span>
+            </h1>
+          </div>
+          <div ref={btnRef} className="btn-container">
+            <button className="down-btn" onClick={handleClick}>
+              <div className="btn-topline" />
+              <h1 className="btn-text">
+                <span className="port-text">portfolio</span>
+              </h1>
+              <div className="btn-bottomline" />
+            </button>
+          </div>
         </div>
-      </div>
-    </div>
+    </AnimatedDiv>
   );
 }
